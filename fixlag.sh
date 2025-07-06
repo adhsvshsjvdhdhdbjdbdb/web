80e6ee8890b9fae1193cc29e213d15494a1598a9
Hackacczggzvsshbsbshj <levietdaithang123@gmail.com>
2025-07-06 22:00:33 +0700

    fixlag.sh

--- fixlag.sh
+++ fixlag.sh
@@ -1026,6 +1026,26 @@ for package in "${packages[@]}"; do
   cmd package compile -m speed-profile -f "$package"
 done
 }> /dev/null 2>&1  
+size=$(wm size | grep -oE '[0-9]+x[0-9]+')
+dpi=$(wm density | grep -oE '[0-9]+')
+if [ -z "$dpi" ]; then
+  echo "❌ Không lấy được DPI. Thoát..."
+  exit 1
+fi
+width=$(echo "$size" | cut -d'x' -f1)
+height=$(echo "$size" | cut -d'x' -f2)
+compare=$(echo "$dpi < 400" | bc)
+if [ "$compare" -eq 1 ]; then
+  scale=1.1
+  echo "🔧 Buff màn hình lên $scale"
+else
+  scale=0.9
+  echo "⚙️ Giảm độ phân giải xuống $scale"
+fi
+new_width=$(echo "$width * $scale" | bc | cut -d'.' -f1)
+new_height=$(echo "$height * $scale" | bc | cut -d'.' -f1)
+wm size ${new_width}x${new_height}
+echo "✅ Kích thước màn hình đã đổi: ${new_width}x${new_height}"
 echo "$DIVIDER"
 echo "$STICKER_COMPLETION$(pad_text "OPTIMIZATION COMPLETED")"
 echo "$SECTION_DIV"
