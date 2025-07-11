19c444288505828d47a2881fde31bb61838867bc
Hackacczggzvsshbsbshj <levietdaithang123@gmail.com>
2025-07-11 21:52:35 +0700

    50k.sh

--- 50k.sh
+++ 50k.sh
@@ -15,7 +15,7 @@ ping=$(ping -c 1 8.8.8.8 | grep 'time=' | awk -F'time=' '{print $2}' | cut -d' '
 sleep 1
 
 echo "========================================="
-echo "|          Module: ZERO V2              |"
+echo "|          Module: ZERO V3              |"
 echo "========================================="
 printf "| Dev         : %-22s |\n" "$dev"
 printf "| Status      : %-22s |\n" "No Root"
@@ -40,7 +40,7 @@ echo "→ [Giảm Rung tâm]"
 echo "→ [Tối ưu trò chơi]"
 echo "→ [Dọn dẹp bộ nhớ]"
 echo "========================================="
-echo " * Wait For Install * "
+echo " * Bắt đầu chạy code * "
 refresh_rate=$(dumpsys SurfaceFlinger | grep refresh-rate | awk -F': ' '{print $2}' | awk '{print int($1+0.5)}')
 echo "$PROGRESS_DIV Display Refresh Rate: ${refresh_rate}Hz"
 
@@ -119,10 +119,6 @@ setprop debug.sf.region_sampling_duration_ns $phazev4
 setprop debug.sf.region_sampling_timer_timeout_ns $phazev7
 setprop debug.sf.region_sampling_period_ns $phazev6
 setprop debug.sf.phase_offset_threshold_for_next_vsync_ns $phazev6
-
-echo "✅ Đã áp dụng tối ưu tương thích cho ${refresh_rate}Hz (không gây văng app)."
-
-
 echo "  [1/3] Applied Display Optimization for ${refresh_rate}Hz"
 #Tối ưu ram
 get_ram_gb() {
@@ -259,7 +255,408 @@ main() {
 }
 
 main
-
+log() {
+setprop log.tag.FA WARN
+setprop log.tag.AF::MmapTrack WARN
+setprop log.tag.AF::OutputTrack WARN
+setprop log.tag.AF::PatchRecord WARN
+setprop log.tag.AF::PatchTrack WARN
+setprop log.tag.AF::RecordHandle WARN
+setprop log.tag.AF::RecordTrack WARN
+setprop log.tag.AF::Track WARN
+setprop log.tag.AF::TrackBase WARN
+setprop log.tag.AF::TrackHandle WARN
+setprop log.tag.APM-KpiMonitor WARN
+setprop log.tag.APM-ServiceJ WARN
+setprop log.tag.APM-SessionJ WARN
+setprop log.tag.APM-SessionN WARN
+setprop log.tag.APM-Subscriber WARN
+setprop log.tag.APM::AudioCollections WARN
+setprop log.tag.APM::AudioInputDescriptor WARN
+setprop log.tag.APM::AudioOutputDescriptor WARN
+setprop log.tag.APM::AudioPatch WARN
+setprop log.tag.APM::AudioPolicyEngine WARN
+setprop log.tag.APM::AudioPolicyEngine::Base WARN
+setprop log.tag.APM::AudioPolicyEngine::Config WARN
+setprop log.tag.APM::AudioPolicyEngine::ProductStrategy WARN
+setprop log.tag.APM::AudioPolicyEngine::VolumeGroup WARN
+setprop log.tag.APM::Devices WARN
+setprop log.tag.APM::IOProfile WARN
+setprop log.tag.APM::Serializer WARN
+setprop log.tag.APM::VolumeCurve WARN
+setprop log.tag.APM_AudioPolicyManager WARN
+setprop log.tag.APM_ClientDescriptor WARN
+setprop log.tag.AT WARN
+setprop log.tag.AccountManager WARN
+setprop log.tag.ActivityManager WARN
+setprop log.tag.ActivityManagerService WARN
+setprop log.tag.ActivityTaskManager WARN
+setprop log.tag.ActivityTaskManagerService WARN
+setprop log.tag.AdnRecord WARN
+setprop log.tag.AdnRecordCache WARN
+setprop log.tag.AdnRecordLoader WARN
+setprop log.tag.AirplaneHandler WARN
+setprop log.tag.AlarmManager WARN
+setprop log.tag.AlarmManagerService WARN
+setprop log.tag.AndroidRuntime WARN
+setprop log.tag.AppOps WARN
+setprop log.tag.AudioAttributes WARN
+setprop log.tag.AudioEffect WARN
+setprop log.tag.AudioFlinger WARN
+setprop log.tag.AudioFlinger::DeviceEffectProxy WARN
+setprop log.tag.AudioFlinger::DeviceEffectProxy::ProxyCallback WARN
+setprop log.tag.AudioFlinger::EffectBase WARN
+setprop log.tag.AudioFlinger::EffectChain WARN
+setprop log.tag.AudioFlinger::EffectHandle WARN
+setprop log.tag.AudioFlinger::EffectModule WARN
+setprop log.tag.AudioFlinger_Threads WARN
+setprop log.tag.AudioHwDevice WARN
+setprop log.tag.AudioManager WARN
+setprop log.tag.AudioPolicy WARN
+setprop log.tag.AudioPolicyEffects WARN
+setprop log.tag.AudioPolicyIntefaceImpl WARN
+setprop log.tag.AudioPolicyManager WARN
+setprop log.tag.AudioPolicyService WARN
+setprop log.tag.AudioProductStrategy WARN
+setprop log.tag.AudioRecord WARN
+setprop log.tag.AudioService WARN
+setprop log.tag.AudioSystem WARN
+setprop log.tag.AudioTrack WARN
+setprop log.tag.AudioTrackShared WARN
+setprop log.tag.AudioVolumeGroup WARN
+setprop log.tag.BackupManager WARN
+setprop log.tag.BatteryManager WARN
+setprop log.tag.BatteryStatsService WARN
+setprop log.tag.BluetoothAdapter WARN
+setprop log.tag.BluetoothDevice WARN
+setprop log.tag.BluetoothGattService WARN
+setprop log.tag.BluetoothHidService WARN
+setprop log.tag.BluetoothManager WARN
+setprop log.tag.BluetoothMapService WARN
+setprop log.tag.BluetoothPanService WARN
+setprop log.tag.BluetoothPbapService WARN
+setprop log.tag.BluetoothSapService WARN
+setprop log.tag.BluetoothService WARN
+setprop log.tag.BluetoothSocket WARN
+setprop log.tag.BufferQueueDump WARN
+setprop log.tag.BufferQueueProducer WARN
+setprop log.tag.C2K_AT WARN
+setprop log.tag.C2K_ATConfig WARN
+setprop log.tag.C2K_RILC WARN
+setprop log.tag.CAM2PORT_ WARN
+setprop log.tag.CapaSwitch WARN
+setprop log.tag.CarrierExpressServiceImpl WARN
+setprop log.tag.CarrierExpressServiceImplExt WARN
+setprop log.tag.ClipboardManager WARN
+setprop log.tag.ConnectivityManager WARN
+setprop log.tag.ConnectivityService WARN
+setprop log.tag.ConsumerIrService WARN
+setprop log.tag.ContentManager WARN
+setprop log.tag.CountryDetector WARN
+setprop log.tag.DMC-ApmService WARN
+setprop log.tag.DMC-Core WARN
+setprop log.tag.DMC-DmcService WARN
+setprop log.tag.DMC-EventsSubscriber WARN
+setprop log.tag.DMC-ReqQManager WARN
+setprop log.tag.DMC-SessionManager WARN
+setprop log.tag.DMC-TranslatorLoader WARN
+setprop log.tag.DMC-TranslatorUtils WARN
+setprop log.tag.DSSelector WARN
+setprop log.tag.DSSelectorOP01 WARN
+setprop log.tag.DSSelectorOP02 WARN
+setprop log.tag.DSSelectorOP09 WARN
+setprop log.tag.DSSelectorOP18 WARN
+setprop log.tag.DSSelectorOm WARN
+setprop log.tag.DSSelectorUtil WARN
+setprop log.tag.DataDispatcher WARN
+setprop log.tag.DeviceIdleController WARN
+setprop log.tag.DevicePolicyManager WARN
+setprop log.tag.DevicePolicyManagerService WARN
+setprop log.tag.DisplayManager WARN
+setprop log.tag.DisplayManagerService WARN
+setprop log.tag.DockObserver WARN
+setprop log.tag.DownloadManager WARN
+setprop log.tag.ExternalSimMgr WARN
+setprop log.tag.FastCapture WARN
+setprop log.tag.FastMixer WARN
+setprop log.tag.FastMixerState WARN
+setprop log.tag.FastThread WARN
+setprop log.tag.FragmentManager WARN
+setprop log.tag.FuseDaemon WARN
+setprop log.tag.GAv4 WARN
+setprop log.tag.GraphicsStats WARN
+setprop log.tag.GsmCallTkrHlpr WARN
+setprop log.tag.GsmCdmaConn WARN
+setprop log.tag.GsmCdmaPhone WARN
+setprop log.tag.HardwarePropertiesManager WARN
+setprop log.tag.HardwareService WARN
+setprop log.tag.IAudioFlinger WARN
+setprop log.tag.IMSRILRequest WARN
+setprop log.tag.IMS_RILA WARN
+setprop log.tag.IccCardProxy WARN
+setprop log.tag.IccPhoneBookIM WARN
+setprop log.tag.IccProvider WARN
+setprop log.tag.ImsApp WARN
+setprop log.tag.ImsBaseCommands WARN
+setprop log.tag.ImsCall WARN
+setprop log.tag.ImsCallProfile WARN
+setprop log.tag.ImsCallSession WARN
+setprop log.tag.ImsEcbm WARN
+setprop log.tag.ImsEcbmProxy WARN
+setprop log.tag.ImsManager WARN
+setprop log.tag.ImsPhone WARN
+setprop log.tag.ImsPhoneBase WARN
+setprop log.tag.ImsPhoneCall WARN
+setprop log.tag.ImsService WARN
+setprop log.tag.ImsVTProvider WARN
+setprop log.tag.InputDispatcher WARN
+setprop log.tag.InputManager WARN
+setprop log.tag.InputManagerService WARN
+setprop log.tag.InputMethodManager WARN
+setprop log.tag.InputMethodManagerService WARN
+setprop log.tag.InterfaceManager WARN
+setprop log.tag.IsimFileHandler WARN
+setprop log.tag.IsimRecords WARN
+setprop log.tag.JobScheduler WARN
+setprop log.tag.KeyguardManager WARN
+setprop log.tag.LCM-Subscriber WARN
+setprop log.tag.LIBC2K_RIL WARN
+setprop log.tag.LocationManager WARN
+setprop log.tag.LocationManagerService WARN
+setprop log.tag.LocationProvider WARN
+setprop log.tag.MAPI-CommandProcessor WARN
+setprop log.tag.MAPI-MdiRedirector WARN
+setprop log.tag.MAPI-MdiRedirectorCtrl WARN
+setprop log.tag.MAPI-NetworkSocketConnection WARN
+setprop log.tag.MAPI-SocketConnection WARN
+setprop log.tag.MAPI-SocketListener WARN
+setprop log.tag.MAPI-TranslatorManager WARN
+setprop log.tag.MDM-Subscriber WARN
+setprop log.tag.MTKSST WARN
+setprop log.tag.MTK_APPList WARN
+setprop log.tag.MediaPlayer WARN
+setprop log.tag.MediaPlayerService WARN
+setprop log.tag.MediaRouter WARN
+setprop log.tag.MediaSession WARN
+setprop log.tag.MipcEventHandler WARN
+setprop log.tag.MountService WARN
+setprop log.tag.MtkAdnRecord WARN
+setprop log.tag.MtkCsimFH WARN
+setprop log.tag.MtkEmbmsAdaptor WARN
+setprop log.tag.MtkFactory WARN
+setprop log.tag.MtkGsmCdmaConn WARN
+setprop log.tag.MtkIccCardProxy WARN
+setprop log.tag.MtkIccPHBIM WARN
+setprop log.tag.MtkIccProvider WARN
+setprop log.tag.MtkIccSmsInterfaceManager WARN
+setprop log.tag.MtkImsManager WARN
+setprop log.tag.MtkImsService WARN
+setprop log.tag.MtkIsimFH WARN
+setprop log.tag.MtkPhoneNotifr WARN
+setprop log.tag.MtkPhoneNumberUtils WARN
+setprop log.tag.MtkPhoneSwitcher WARN
+setprop log.tag.MtkRecordLoader WARN
+setprop log.tag.MtkRuimFH WARN
+setprop log.tag.MtkSIMFH WARN
+setprop log.tag.MtkSIMRecords WARN
+setprop log.tag.MtkSmsCbHeader WARN
+setprop log.tag.MtkSmsManager WARN
+setprop log.tag.MtkSmsMessage WARN
+setprop log.tag.MtkSpnOverride WARN
+setprop log.tag.MtkSubCtrl WARN
+setprop log.tag.MtkTelephonyManagerEx WARN
+setprop log.tag.MtkUiccCard WARN
+setprop log.tag.MtkUiccCardApp WARN
+setprop log.tag.MtkUiccCtrl WARN
+setprop log.tag.MtkUsimFH WARN
+setprop log.tag.MtkUsimPhoneBookManager WARN
+setprop log.tag.MwiRIL WARN
+setprop log.tag.NetAgentService WARN
+setprop log.tag.NetAgent_IO WARN
+setprop log.tag.NetLnkEventHdlr WARN
+setprop log.tag.NetworkManagement WARN
+setprop log.tag.NetworkManagementService WARN
+setprop log.tag.NetworkPolicy WARN
+setprop log.tag.NetworkPolicyManagerService WARN
+setprop log.tag.NetworkStats WARN
+setprop log.tag.NetworkTimeUpdateService WARN
+setprop log.tag.NotificationManager WARN
+setprop log.tag.NotificationManagerService WARN
+setprop log.tag.OperatorUtils WARN
+setprop log.tag.PKM-Lib WARN
+setprop log.tag.PKM-MDM WARN
+setprop log.tag.PKM-Monitor WARN
+setprop log.tag.PKM-SA WARN
+setprop log.tag.PKM-Service WARN
+setprop log.tag.PQ_DS WARN
+setprop log.tag.PackageInstaller WARN
+setprop log.tag.PackageManager WARN
+setprop log.tag.PersistentDataBlockManager WARN
+setprop log.tag.Phone WARN
+setprop log.tag.PhoneConfigurationSettings WARN
+setprop log.tag.PhoneFactory WARN
+setprop log.tag.PowerHalAddressUitls WARN
+setprop log.tag.PowerHalMgrImpl WARN
+setprop log.tag.PowerHalMgrServiceImpl WARN
+setprop log.tag.PowerHalWifiMonitor WARN
+setprop log.tag.PowerManager WARN
+setprop log.tag.PowerManagerService WARN
+setprop log.tag.PrintManager WARN
+setprop log.tag.ProcessStats WARN
+setprop log.tag.ProxyController WARN
+setprop log.tag.RFX WARN
+setprop log.tag.RIL WARN
+setprop log.tag.RIL-Fusion WARN
+setprop log.tag.RIL-Netlink WARN
+setprop log.tag.RIL-Parcel WARN
+setprop log.tag.RIL-SocListen WARN
+setprop log.tag.RIL-Socket WARN
+setprop log.tag.RILC WARN
+setprop log.tag.RILC-OP WARN
+setprop log.tag.RILD WARN
+setprop log.tag.RILMD2-SS WARN
+setprop log.tag.RIL_UIM_SOCKET WARN
+setprop log.tag.RadioManager WARN
+setprop log.tag.RfxAction WARN
+setprop log.tag.RfxBaseHandler WARN
+setprop log.tag.RfxChannelMgr WARN
+setprop log.tag.RfxCloneMgr WARN
+setprop log.tag.RfxContFactory WARN
+setprop log.tag.RfxController WARN
+setprop log.tag.RfxDebugInfo WARN
+setprop log.tag.RfxDisThread WARN
+setprop log.tag.RfxFragEnc WARN
+setprop log.tag.RfxHandlerMgr WARN
+setprop log.tag.RfxIdToMsgId WARN
+setprop log.tag.RfxIdToStr WARN
+setprop log.tag.RfxMainThread WARN
+setprop log.tag.RfxMclDisThread WARN
+setprop log.tag.RfxMclMessenger WARN
+setprop log.tag.RfxMclStatusMgr WARN
+setprop log.tag.RfxMessage WARN
+setprop log.tag.RfxObject WARN
+setprop log.tag.RfxOpUtils WARN
+setprop log.tag.RfxRilAdapter WARN
+setprop log.tag.RfxRilUtils WARN
+setprop log.tag.RfxRoot WARN
+setprop log.tag.RfxStatusMgr WARN
+setprop log.tag.RfxTimer WARN
+setprop log.tag.RilClient WARN
+setprop log.tag.RilOemClient WARN
+setprop log.tag.RilOpProxy WARN
+setprop log.tag.RmmCapa WARN
+setprop log.tag.RmmCommSimOpReq WARN
+setprop log.tag.RmmDcEvent WARN
+setprop log.tag.RmmDcPdnManager WARN
+setprop log.tag.RmmDcUrcHandler WARN
+setprop log.tag.RmmDcUtility WARN
+setprop log.tag.RmmEccNumberReqHdlr WARN
+setprop log.tag.RmmEccNumberUrcHandler WARN
+setprop log.tag.RmmEmbmsReq WARN
+setprop log.tag.RmmEmbmsUrc WARN
+setprop log.tag.RmmImsCtlReqHdl WARN
+setprop log.tag.RmmImsCtlUrcHdl WARN
+setprop log.tag.RmmMwi WARN
+setprop log.tag.RmmNwAsyncHdlr WARN
+setprop log.tag.RmmNwHdlr WARN
+setprop log.tag.RmmNwNrtReqHdlr WARN
+setprop log.tag.RmmNwRTReqHdlr WARN
+setprop log.tag.RmmNwRatSwHdlr WARN
+setprop log.tag.RmmNwReqHdlr WARN
+setprop log.tag.RmmNwUrcHdlr WARN
+setprop log.tag.RmmOemHandler WARN
+setprop log.tag.RmmOpRadioReq WARN
+setprop log.tag.RmmPhbReq WARN
+setprop log.tag.RmmPhbUrc WARN
+setprop log.tag.RmmRadioReq WARN
+setprop log.tag.RmmSimBaseHandler WARN
+setprop log.tag.RmmSimCommReq WARN
+setprop log.tag.RmmSimCommUrc WARN
+setprop log.tag.RmmWp WARN
+setprop log.tag.RtmCapa WARN
+setprop log.tag.RtmCommSimCtrl WARN
+setprop log.tag.RtmDC WARN
+setprop log.tag.RtmEccNumberController WARN
+setprop log.tag.RtmEmbmsAt WARN
+setprop log.tag.RtmEmbmsUtil WARN
+setprop log.tag.RtmIms WARN
+setprop log.tag.RtmImsConference WARN
+setprop log.tag.RtmImsConfigController WARN
+setprop log.tag.RtmImsDialog WARN
+setprop log.tag.RtmModeCont WARN
+setprop log.tag.RtmMwi WARN
+setprop log.tag.RtmNwCtrl WARN
+setprop log.tag.RtmPhb WARN
+setprop log.tag.RtmRadioConfig WARN
+setprop log.tag.RtmRadioCont WARN
+setprop log.tag.RtmWp WARN
+setprop log.tag.SIMRecords WARN
+setprop log.tag.SQLiteQueryBuilder WARN
+setprop log.tag.SensorManager WARN
+setprop log.tag.ServiceManager WARN
+setprop log.tag.SimSwitchOP01 WARN
+setprop log.tag.SimSwitchOP02 WARN
+setprop log.tag.SimSwitchOP18 WARN
+setprop log.tag.SlotQueueEntry WARN
+setprop log.tag.SpnOverride WARN
+setprop log.tag.StatusBarManagerService WARN
+setprop log.tag.StorageManager WARN
+setprop log.tag.SurfaceFlinger WARN
+setprop log.tag.SystemServer WARN
+setprop log.tag.Telecom WARN
+setprop log.tag.TelephonyManager WARN
+setprop log.tag.TelephonyRegistry WARN
+setprop log.tag.ThermalManager WARN
+setprop log.tag.ToneGenerator WARN
+setprop log.tag.UiccCard WARN
+setprop log.tag.UiccController WARN
+setprop log.tag.UsbHostManager WARN
+setprop log.tag.UsbManager WARN
+setprop log.tag.UxUtility WARN
+setprop log.tag.VT WARN
+setprop log.tag.VibratorService WARN
+setprop log.tag.VpnManager WARN
+setprop log.tag.VsimAdaptor WARN
+setprop log.tag.WORLDMODE WARN
+setprop log.tag.WallpaperManager WARN
+setprop log.tag.WfoApp WARN
+setprop log.tag.WifiManager WARN
+setprop log.tag.WindowManager WARN
+setprop log.tag.WindowManagerService WARN
+setprop log.tag.WpfaCcciDataHeaderEncoder WARN
+setprop log.tag.WpfaCcciReader WARN
+setprop log.tag.WpfaCcciSender WARN
+setprop log.tag.WpfaControlMsgHandler WARN
+setprop log.tag.WpfaDriver WARN
+setprop log.tag.WpfaDriverAccept WARN
+setprop log.tag.WpfaDriverAdapter WARN
+setprop log.tag.WpfaDriverDeReg WARN
+setprop log.tag.WpfaDriverMessage WARN
+setprop log.tag.WpfaDriverRegFilter WARN
+setprop log.tag.WpfaDriverULIpPkt WARN
+setprop log.tag.WpfaDriverUtilis WARN
+setprop log.tag.WpfaDriverVersion WARN
+setprop log.tag.WpfaFilterRuleReqHandler WARN
+setprop log.tag.WpfaParsing WARN
+setprop log.tag.WpfaRingBuffer WARN
+setprop log.tag.WpfaRuleContainer WARN
+setprop log.tag.WpfaRuleRegister WARN
+setprop log.tag.WpfaShmAccessController WARN
+setprop log.tag.WpfaShmReadMsgHandler WARN
+setprop log.tag.WpfaShmSynchronizer WARN
+setprop log.tag.WpfaShmWriteMsgHandler WARN
+setprop log.tag.brevent.event WARN
+setprop log.tag.libPowerHal WARN
+setprop log.tag.libfuse WARN
+setprop log.tag.mipc_lib WARN
+setprop log.tag.mtkpower@impl WARN
+setprop log.tag.mtkpower_client WARN
+setprop log.tag.trm_lib WARN
+setprop log.tag.wpfa_iptable_android WARN
+setprop log.tag.Networklogger WARN
+setprop log.tag.AudioFlinger::DeviceEffectProxy WARN
+}
+log > /dev/null 2>&1  
 echo "[3/3] Log Suppression Applied "
 # Performance props
 pe() {
@@ -272,7 +669,9 @@ settings put system POWER_SAVE_PRE_SYNCHRONIZE_ENABLE 0
 settings put global GPUTUNER_SWITCH true
 settings put global CPUTUNER_SWITCH true
 settings put global performance_profile high_performance
+cmd power set-adaptive-power-saver-enabled false
 cmd power set-fixed-performance-mode-enabled true
+cmd power set-mode 0
 setprop debug.javafx.animation.fullspeed true
 setprop debug.javafx.animation.framerate 165
 setprop debug.javafx.animation.fullframe 1
@@ -353,6 +752,7 @@ setprop debug.performance.profile 1
 setprop debug.perf.profile 1
 setprop debug.hwui.use_layer_renderer true
 setprop debug.hwui.target_cpu_time_percent 100
+dumpsys battery set level 100
 setprop debug.hwui.target_gpu_time_percent 100
 }
 pe > /dev/null 2>&1  
@@ -375,33 +775,10 @@ for package in "${packages[@]}"; do
 done
 }
 game > /dev/null 2>&1  
-buff() {
-size=$(wm size | grep -oE '[0-9]+x[0-9]+')
-dpi=$(wm density | grep -oE '[0-9]+')
-if [ -z "$dpi" ]; then
-  echo "❌ Không lấy được DPI. Thoát..."
-  exit 1
-fi
-width=$(echo "$size" | cut -d'x' -f1)
-height=$(echo "$size" | cut -d'x' -f2)
-compare=$(echo "$dpi < 400" | bc)
-if [ "$compare" -eq 1 ]; then
-  scale=1.1
-  echo "🔧 Buff màn hình lên $scale"
-else
-  scale=0.9
-  echo "⚙️ Giảm độ phân giải xuống $scale"
-fi
-new_width=$(echo "$width * $scale" | bc | cut -d'.' -f1)
-new_height=$(echo "$height * $scale" | bc | cut -d'.' -f1)
-wm size ${new_width}x${new_height}
-}
-buff > /dev/null 2>&1  
-echo "✅ Kích thước màn hình đã đổi: ${new_width}x${new_height}"
 echo "Success☑️" 
 echo "Vui lòng không khởi động lại máy sẽ mất tác dụng"
 echo "Vào game test thôi🎮"
-cmd notification post -t "🔥ZERO V2🔥" -S inbox \
+cmd notification post -t "🔥ZERO V3🔥" -S inbox \
     --line "Tăng hiệu suất cao ☑" \
     --line "Ổn định thiết bị, giảm nóng và lag ☑" \
     --line "Tăng nhạy ổn định☑" \
